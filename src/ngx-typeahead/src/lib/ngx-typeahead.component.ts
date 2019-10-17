810e538fe58fe85e8d26fe3e52f22e0266b541d4
MADCAZ <9088059+MADCAZ@users.noreply.github.com>
2019-10-17 17:30:48 +0700

    initial project

---
+++ src/ngx-typeahead/src/lib/ngx-typeahead.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-typeahead',
+  template: `
+    <p>
+      ngx-typeahead works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxTypeaheadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/ngx-typeahead/src/lib/ngx-typeahead.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxTypeaheadComponent } from './ngx-typeahead.component';
+
+
+
+@NgModule({
+  declarations: [NgxTypeaheadComponent],
+  imports: [
+  ],
+  exports: [NgxTypeaheadComponent]
+})
+export class NgxTypeaheadModule { }
